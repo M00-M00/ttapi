75fac7ea195216db55692bdcc574492be9dbddc0
Eugene Dimchenko <edimchenko@gmail.com>
2020-09-30 12:06:43 +0100

    Changed CLI in main, added name_json(), remove naming from petrol.py, added counter to detailed & nearby

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import argparse
 from petrol import PetrolTomTomApi
+import data_structures as ds
 
 
 parser = argparse.ArgumentParser()
@@ -20,6 +21,22 @@ json= args.json
 radius = args.radius
 stop = args.detailed_stop
 
+def name_json():
+    if mode == "Nearby":
+        if json == None:
+            filename = brand + "_" + str(radius) +  "_Nearby_Petrol.json"
+        else:
+            filename = json.split(".")[0] + "_" + str(radius) +  "_Nearby_Petrol.json"
+
+    elif mode == "Detailed":
+        if json == None:
+            filename = brand  +  "_Detailed_Petrol.json"
+        else:
+            filename = json.split(".")[0]  +  "_Detailed_Petrol.json"
+    return filename
+
+
+
 
 
 
@@ -30,19 +47,24 @@ if __name__ == "__main__":
         if brand == None:
             print("No Brand selected, enter brand")
             brand = str(input())
-            api.get_total_by_brand(brand)
+        api.get_total_by_brand(brand)
+        filename = brand + ".json"
+        ds.save_json(filename, api.results_dict)
     else:    
         if json == None:
-            print("No json selected, enter what brand to search first")
-            brand = str(input())
+            if brand == None:
+                print("No json selected, enter what brand to search first")
+                brand = str(input())
             api.get_total_by_brand([brand])
         else:
-            results_dict = api.load_petrol_stations(json)
-            api.ids = [a for a in results_dict]
+            api.results_dict = api.load_petrol_stations(json)
+            api.ids = [a for a in api.results_dict]
 
         if mode == "Detailed":
             if detailed == None:
                 api.search_all_detailed(api.ids)
+                filename = name_json()
+                ds.save_json(filename, api.detailed_results)
             else: 
                 api.detailed_results = api.load_detailed_results(detailed)
                 start = len(api.detailed_results)
@@ -52,13 +74,20 @@ if __name__ == "__main__":
                 else:
                     ids_to_scrape = api.ids[start:]
                 api.search_all_detailed(ids_to_scrape)
+                filename = name_json()
+                ds.save_json(filename, api.detailed_results)
 
         elif mode =="Nearby":
             if radius == None:
                 print("Enter radius for nearby search") 
                 radius = str(input())
+                api.search_all_nearbies(radius = radius, ids = api.ids)
+                filename = name_json()
+                ds.save_json(filename, api.nearby_results)
             else:
                 api.search_all_nearbies(radius = radius, ids = api.ids)
+                filename = name_json()
+                ds.save_json(filename, api.nearby_results)
 
         
 
