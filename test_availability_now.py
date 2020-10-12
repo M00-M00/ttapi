ed9b147ded4dcefaa8abf53c0b634dfb3eeb75e4
Eugene Dimchenko <edimchenko@gmail.com>
2020-10-12 04:14:42 +0100

    changed scheduler hours in test_availability_now.py

--- test_availability_now.py
+++ test_availability_now.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour="3-20", minute = "51")
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour="8-20")
 def scheduled_job():
     api = EV_Search()
     api.temporary_ev_availability([8898,7654], "availability_test.json")
